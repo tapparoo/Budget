bfde8a060afef1bde000f1bc1327354fb56302f5
taptaptapparoo <37118442+taptaptapparoo@users.noreply.github.com>
2018-09-17 11:07:51 -0600

    Path

--- js/display income.js
+++ js/display income.js
@@ -4,7 +4,7 @@ const renderIncomeTable = function(){
     incomes.forEach(pay => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td><a href="/Add-Edit Income.html#${pay.id}">${pay.name}</a></td>
+            <td><a href="https://taptaptapparoo.github.io/Budget/Add-Edit Income.html#${pay.id}">${pay.name}</a></td>
             <td>${pay.value}</td>
             <td>${pay.frequency}</td>`;        
         payTableBody.appendChild(row);
